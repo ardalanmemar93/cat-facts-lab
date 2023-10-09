ab991ff99be9a211a82d4151e271b3791496d571
ardalanmemar93 <ardalanmemar@gmail.com>
2023-10-08 18:57:26 -0700

    finishing cat-facts-lab

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const fetch = require('axios');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'PRRRRRRRRRR' });
+});
+// GET /cats/facts
+router.get('/cats/fact', async function(req, res) {
+  const response = await fetch('https://catfact.ninja/fact');
+  const catFacts = response.data.fact;
+  res.render('facts', { catFacts });
+})
+
+module.exports = router;
